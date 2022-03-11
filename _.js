304246f93ab75652698289050a0f6fbbac79fd06
Michaël Laurac <michaellaurac@gmail.com>
2022-03-11 15:14:43 +0100

    Implementing the clamp method

--- _.js
+++ _.js
@@ -1,4 +1,8 @@
-_ = {};
+_ = {
+  clamp: function(inputNumber, lowerBound, upperBound){
+    return Math.min(Math.max(inputNumber, lowerBound), upperBound);
+  }
+};
 
 // Do not write or modify code below this line.
 module.exports = _;
