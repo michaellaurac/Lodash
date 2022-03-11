e6a4d23dff3dfc56566055e94acfee3ca22da44b
Michaël Laurac <michaellaurac@gmail.com>
2022-03-11 15:40:48 +0100

    Made the clamp method more robust with lower bound is undefined and added an inRange method

--- _.js
+++ _.js
@@ -1,6 +1,18 @@
 _ = {
-  clamp: function(inputNumber, lowerBound, upperBound){
-    return Math.min(Math.max(inputNumber, lowerBound), upperBound);
+  clamp: function(inputNumber, lowerBound, upperBound) {
+    return lowerBound ? Math.min(Math.max(inputNumber, lowerBound), upperBound) : Math.min(inputNumber, upperBound);
+  },
+  inRange(inputNumber, startValue, endValue) {
+    if (!endValue) {
+      endValue = startValue;
+      startValue = 0;
+    }
+    if (startValue > endValue ) {
+      let tempValue = startValue;
+      startValue = endValue;
+      endValue = tempValue;
+    }
+    return startValue <= inputNumber && inputNumber < endValue
   }
 };
 
